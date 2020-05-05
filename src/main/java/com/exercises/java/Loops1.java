459c9e24a394ec2e8ee4ca4ba2bcfac01447a042
Pedro Silva <pedrro2@hotmail.com>
2020-05-05 17:52:21 -0300

    java exercises

---
+++ src/main/java/com/exercises/java/Loops1.java
@@ -0,0 +1,10 @@
+package com.exercises.java;
+
+public class Loops1 {
+    public static void main(String[] args) {
+        int n = 2;
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%s x %s = %d\n", n, i, n * i);
+        }
+    }
+}
