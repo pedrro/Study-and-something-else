8bd6fb10be756e765f8da526ecd18289a420bc2e
Pedro Silva <pedrro2@hotmail.com>
2020-05-11 13:56:18 -0300

    EOF

---
+++ src/main/java/com/exercises/java/EndOfFile.java
@@ -0,0 +1,15 @@
+package com.exercises.java;
+
+import java.util.Scanner;
+
+public class EndOfFile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int count = 0;
+        while(sc.hasNext()) {
+            count++;
+
+            System.out.println(count + " " + sc.nextLine());
+        }
+    }
+}
