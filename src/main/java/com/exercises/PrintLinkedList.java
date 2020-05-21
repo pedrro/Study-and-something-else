ed73d5ad8194ea3fb8df405aeee46ffb284dc6b0
Pedro Silva <pedrro2@hotmail.com>
2020-05-21 14:21:25 -0300

    Print linked list

---
+++ src/main/java/com/exercises/PrintLinkedList.java
@@ -0,0 +1,72 @@
+package com.exercises;
+
+import java.util.Scanner;
+
+public class PrintLinkedList {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    // Complete the printLinkedList function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static void printLinkedList(SinglyLinkedListNode head) {
+        if(head.next != null) {
+            System.out.println(head.data);
+            printLinkedList(head.next);
+        } else {
+            System.out.println(head.data);
+        }
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        SinglyLinkedList llist = new SinglyLinkedList();
+
+        llist.insertNode(16);
+        llist.insertNode(13);
+
+
+        printLinkedList(llist.head);
+
+        scanner.close();
+    }
+}
