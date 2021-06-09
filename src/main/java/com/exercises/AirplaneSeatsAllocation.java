2b8a671843d2bda02593a94e63e93b7451ec87f7
Pedro Silva <pedrro2@hotmail.com>
2021-06-09 19:36:44 -0300

    airplane seats refactor

--- src/main/java/com/exercises/AirplaneSeatsAllocation.java
+++ src/main/java/com/exercises/AirplaneSeatsAllocation.java
@@ -1,65 +1,46 @@
 package com.exercises;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class AirplaneSeatsAllocation {
 
-    static int solution(int n, String string) {
-        String[] seats = string.split(" ");
-
-        HashMap<Character, Integer> airplaneSeats = new HashMap<>();
-        airplaneSeats.put('A', 0);
-        airplaneSeats.put('B', 0);
-        airplaneSeats.put('C', 0);
-
-        airplaneSeats.put('D', 0);
-        airplaneSeats.put('E', 0);
-        airplaneSeats.put('F', 0);
-        airplaneSeats.put('G', 0);
-
-        airplaneSeats.put('H', 0);
-        airplaneSeats.put('J', 0);
-        airplaneSeats.put('K', 0);
-
-        ArrayList<HashMap<Character, Integer>> rows = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            rows.add(airplaneSeats);
-        }
-
-        for (String seat: seats) {
-            int row = Character.getNumericValue(seat.charAt(0));
-            char place = seat.charAt(1);
-
-            rows.get(row - 1).put(place, 1);
-        }
-
-        int result = 0;
-        for (int row = 0; row < rows.size(); row++) {
-            HashMap<Character, Integer> populatedRow = rows.get(row);
-
-            if(populatedRow.get('A') + populatedRow.get('B') + populatedRow.get('C') == 0) {
-                result++;
-            }
-
-            if(populatedRow.get('D') + populatedRow.get('E') + populatedRow.get('F') + populatedRow.get('G') <= 1) {
-                result++;
-            }
-
-            if(populatedRow.get('H') + populatedRow.get('J') + populatedRow.get('K') == 0) {
-                result++;
-            }
-        }
-
-        return result;
-    }
-
     public static void main(String[] args) {
 
-        String s = "1A 2F 1C";
+        String[] s = "1A 2F 1C".split(" ");
+        int sum = Arrays.stream(s)
+                .collect(Collectors.groupingBy(value -> value.substring(0, 1)))
+                .values()
+                .stream()
+                .map(strings -> strings
+                        .stream()
+                        .map(s1 -> s1.charAt(1))
+                        .map(s2 -> Character.getNumericValue(s2) - 10)
+                        .collect(Collectors.toList()))
+                .map(a -> fun(a))
+                .mapToInt(i -> i)
+                .sum();
 
-        int solution = solution(2, s);
+        System.out.println(sum);
+    }
 
-        System.out.println(solution);
+    static int fun(List<Integer> ints){
+        HashSet<Integer> used = new HashSet<>(ints);
+        int families =0;
+        int free =0;
+        for(int i =0;i<10; i++) {
+            if (used.contains(i)) {
+                free = 0;
+            } else {
+                free++;
+            }
+            if (free == 3) {
+                families++;
+                free =0;
+            }
+        }
+        return families;
     }
 }
